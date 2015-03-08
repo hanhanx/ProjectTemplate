7a5613abc16f096138dbb51050a7c058dbbbd37d
hanhanx <hanhanx@gmail.com>
2015-03-08 16:15:59 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -184,7 +184,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         open: false
       },
       dev: {
