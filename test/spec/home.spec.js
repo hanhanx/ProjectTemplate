54a306fc7a13a7c54a3ead6b8f71deb75a8c9b62
JKHero <hanhanx@gmail.com>
2015-03-08 16:03:57 -0400

    e2e testing home page clicks

--- test/spec/home.spec.js
+++ test/spec/home.spec.js
@@ -1,12 +1,22 @@
 'use strict';
 
 var bwlog = require('../lib/browserlog.js');
-describe('test homepage', function() {
+ddescribe('test homepage', function() {
     beforeEach(function() {
         browser.get('http://localhost:9000/');
     });
     it('logging', function() {
         browser.waitForAngular();
+        expect(element(by.css('.jumbotron')).isPresent()).toBe(true);
+
+        element(by.cssContainingText('.header>ul>li>a', 'About')).click();
+        expect(element(by.cssContainingText('div>p', 'This is the about view.')).isPresent()).toBe(true);
+
+        element(by.cssContainingText('.header>ul>li>a', 'Contact')).click();
+        expect(element(by.css('[ng-view]')).isPresent()).toBe(false);
+
+        element(by.cssContainingText('.header>ul>li>a', 'Home')).click();
+        expect(element(by.css('.jumbotron')).isPresent()).toBe(true);
     });
 
     afterEach(function() {
